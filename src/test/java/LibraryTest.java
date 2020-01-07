5ce51d35099909619aa1318aca3a8b2d8831fd42
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-07 20:21:17 +0000

    creates library test file

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before() {
+        library = new Library();
+    }
+
+    @Test
+    public void libraryStartsWithNoBooks() {
+        assertEquals(0, library.countBooks());
+    }
 }
+
