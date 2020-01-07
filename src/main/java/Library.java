5502cee9472bff1d1b0b07013e03cdab0fa32487
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-07 20:25:14 +0000

    library starts wiht no books test passing

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
 
+    private ArrayList<Book> collection;
+
+    public Library() {
+        this.collection = new ArrayList<Book>();
+    }
+
+    public int countBooks() {
+        return this.collection.size();
+    }
 }
